c02f62a6c083419eb472b060b5b4d2e87eb5f7b6
jiaojing <baojing.jiao@gmail.com>
2012-07-02 18:17:15 +0800

    some code added

--- src/main/java/com/github/nessava/DB.java
+++ src/main/java/com/github/nessava/DB.java
@@ -1,6 +1,50 @@
 package com.github.nessava;
 
 public class DB {
+	private final String dir;
+	private final LLRU llru;
+	private final Index index;
+
+	public static DB open(String dir) {
+		return new DB(dir);
+	}
+
+	private DB(String dir) {
+		this.dir = dir;
+		this.llru = new LLRU();
+		this.index = new Index(dir);
+	}
+
+	public byte[] get(byte[] key) {
+		byte[] value = llru.get(key);
+		if (value == null) {
+			value = index.get(key);
+			if (value != null) {
+				llru.set(key, value);
+			}
+		}
+		return value;
+	}
+
+	public boolean exist(byte[] key) {
+		return false;
+	}
+
+	public boolean add(byte[] key, byte[] value) {
+		llru.remove(key,value);
+		return index.add(key,value);
+	}
+
+	public boolean remove(byte[] key) {
+		return false;
+	}
+
+	public void close() {
+	}
+
+	public String info() {
+		return null;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("hello nessava!");
